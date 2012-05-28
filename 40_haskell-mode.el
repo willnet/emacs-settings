fe8ecb29c6286721e036370081c6efa6de1530e3
willnet <netwillnet@gmail.com>
2012-05-28 16:25:54 +0900

    Add haskell-mode

---
+++ 40_haskell-mode.el
@@ -0,0 +1,8 @@
+(load "~/Dropbox/emacs-lisp/vendor/haskell-mode/haskell-mode.el")
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
