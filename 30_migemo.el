f2bfc172557e00750b50bfacf5cf41dcb3291c5f
willnet <netwillnet@gmail.com>
2012-05-28 16:24:48 +0900

    Add migemo

---
+++ 30_migemo.el
@@ -0,0 +1,10 @@
+;; http://d.hatena.ne.jp/ground256/20111008/1318063872
+(require 'migemo)
+(setq migemo-command "/usr/local/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(setq migemo-regex-dictionary nil)
+(load-library "migemo")
+(migemo-init)
