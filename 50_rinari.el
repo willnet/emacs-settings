fcdc29bf15c90bff8b4fa501fc39f0f42c0850ec
willnet <netwillnet@gmail.com>
2012-06-07 11:43:00 +0900

    Modify rinari settings

--- 50_rinari.el
+++ 50_rinari.el
@@ -1,6 +1,6 @@
 ;; Interactively Do Things (highly recommended, but not strictly required)
-(require 'ido)
-(ido-mode t)
+;; (require 'ido)
+;; (ido-mode t)
 
 ;; Rinari
 (require 'rinari)
@@ -9,16 +9,60 @@
           (lambda () (rinari-launch)))
 (setq rinari-tags-file-name "TAGS")
 
+(setf
+ my-rinari-jump-schema
+ '((my-stylesheet "y" ((t . "app/assets/stylesheets/.*")
+                    (t . "public/stylesheets/.*")) nil)
+   (my-javascript "j" ((t . "app/assets/javascripts/.*")
+                    (t . "public/javascripts/.*")) nil)
+   (my-fabrication "f" ((t . "spec/fabricators/.*")) nil)
+   (my-rspec
+    "t"
+    (("app/models/\\1.rb"                      . "spec/models/\\1_spec.rb")
+     ("app/controllers/\\1.rb"                 . "spec/controllers/\\1_spec.rb")
+     ("app/views/\\1\\..*"                     . "spec/views/\\1_spec.rb")
+     ("lib/\\1.rb"                             . "spec/libs/\\1_spec.rb")
+     ("db/migrate/.*_create_\\1.rb"            . "spec/models/\\1_spec.rb")
+     ("config/routes.rb"                       . "spec/routing/.*\\.rb")
+     (t                                        . "spec/.*\\.rb"))
+    t)
+   (my-decorator
+    "d"
+    (("app/models/\\1.rb"                      . "app/decorators/\\1_decorator.rb")
+     ("app/controllers/\\1.rb"                 . "app/decorators/\\1_decorator.rb")
+     ("app/views/\\1\\..*"                     . "app/decorators/\\1_decorator.rb")
+     ("db/migrate/.*_create_\\1.rb"            . "app/decorators/\\1_decorator.rb")
+     (t                                        . "app/decorators/.*\\.rb"))
+    t)
+
+   (my-request-rspec "r" ((t . "spec/requests/.*")) nil)
+   ))
+
+(add-to-list 'auto-mode-alist '("\\.text\\.erb$" . rhtml-mode))
+
+(rinari-apply-jump-schema my-rinari-jump-schema)
+
 (define-key rinari-minor-mode-map (kbd "C-c m") 'rinari-find-model)
+(define-key rinari-minor-mode-map (kbd "C-c M") 'rinari-find-mailer)
 (define-key rinari-minor-mode-map (kbd "C-c c") 'rinari-find-controller)
 (define-key rinari-minor-mode-map (kbd "C-c o") 'rinari-find-configuration)
+(define-key rinari-minor-mode-map (kbd "C-c a") 'rinari-find-application)
 (define-key rinari-minor-mode-map (kbd "C-c e") 'rinari-find-environment)
 (define-key rinari-minor-mode-map (kbd "C-c h") 'rinari-find-helper)
 (define-key rinari-minor-mode-map (kbd "C-c v") 'rinari-find-view)
 (define-key rinari-minor-mode-map (kbd "C-c i") 'rinari-find-migration)
 (define-key rinari-minor-mode-map (kbd "C-c l") 'rinari-find-lib)
-(define-key rinari-minor-mode-map (kbd "C-c y") 'rinari-find-styleseet)
-(define-key rinari-minor-mode-map (kbd "C-c j") 'rinari-find-javascript)
+(define-key rinari-minor-mode-map (kbd "C-c r") 'rinari-find-my-request-rspec)
+(define-key rinari-minor-mode-map (kbd "C-c t") 'rinari-find-my-rspec)
+(define-key rinari-minor-mode-map (kbd "C-c f") 'rinari-find-my-fabrication)
+(define-key rinari-minor-mode-map (kbd "C-c y") 'rinari-find-my-stylesheet)
+(define-key rinari-minor-mode-map (kbd "C-c j") 'rinari-find-my-javascript)
+(define-key rinari-minor-mode-map (kbd "C-c d") 'rinari-find-my-decorator)
 (define-key rinari-minor-mode-map (kbd "C-c x") 'rinari-extract-partial)
+(define-key rinari-minor-mode-map (kbd "C-c p") 'rinari-console)
+(define-key rinari-minor-mode-map (kbd "C-c C-c c") 'rinari-cap)
 
-
+(defun my-find-gemfile ()
+  (interactive)
+  (find-file (concat (rinari-root) "Gemfile")))
+(define-key rinari-minor-mode-map (kbd "C-c g") 'my-find-gemfile)
