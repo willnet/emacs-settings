65571e4f856f03567a7fa648de667915f1d78c94
willnet <netwillnet@gmail.com>
2012-08-20 17:50:47 +0900

    Modify settings for updating yasnippet
    * add C-i keybind
    * delete anything-yasnippet setting because it doesn't work

--- 30_yasnippet.el
+++ 30_yasnippet.el
@@ -1,13 +1,10 @@
 ;; yasnippet
-(require 'yasnippet)
-(require 'anything-c-yasnippet)
-(yas/initialize)
-(yas/load-directory
- (expand-file-name "~/Dropbox/emacs-lisp/elisp/yasnippet/snippets"))
+(require 'yasnippet-config)
 (require 'dropdown-list)
-(setq yas/prompt-functions '(yas/dropdown-prompt
-                             yas/ido-prompt
-                             yas/completing-prompt))
+(yas/global-mode 1)
+(call-interactively 'yas/reload-all) ;workaround
+;; anything-complete.elを使っているなら yas/completing-prompt のみでもよい
+(setq yas/prompt-functions
+'(yas/dropdown-prompt yas/completing-prompt yas/ido-prompt yas/no-prompt))
 
-(setq anything-c-yas-space-match-any-greedy t) ;スペース区切りで絞り込めるようにする デフォルトは nil
-(global-set-key (kbd "C-x y") 'anything-c-yas-complete)
+(setq yas-trigger-key "C-i")
