e49ee05d7bf21131d7c2f874409ee3ceb7197dff
willnet <netwillnet@gmail.com>
2012-10-15 00:14:04 +0900

    Enable ruby-mode on Rakefile

--- 40_ruby.el
+++ 40_ruby.el
@@ -12,6 +12,7 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
