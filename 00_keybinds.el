d2931f81d637af645055e8908c8896985de9abe8
willnet <netwillnet@gmail.com>
2012-10-15 00:03:55 +0900

    Add some keybinds

--- 00_keybinds.el
+++ 00_keybinds.el
@@ -1,5 +1,8 @@
 ;; バックスペース
-(define-key global-map "\C-h" 'delete-backward-char)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; 直前の単語を削除
+(global-set-key (kbd "M-h") 'backward-kill-word)
 
 ;; ヘルプ
 (define-key global-map (kbd "C-x ?") 'help-command)
@@ -16,14 +19,11 @@
 ;; C-x oの逆をC-x pに割り当てる
 (global-set-key (kbd "C-x p") (lambda () (interactive) (other-window -1)))
 
-;; フルスクリーン
-(define-key global-map (kbd "C-x m") 'ns-toggle-fullscreen)
-
 ;; ウィンドウを移る
 (define-key global-map (kbd "C-o") 'other-window)
 
 ;; rgrep
-(define-key global-map (kbd "C-c r") 'rgrep)
+(define-key global-map (kbd "C-x g") 'rgrep)
 
 ;; anything-rurema
 (define-key global-map (kbd "C-c a") 'anything-rurima)
@@ -31,3 +31,13 @@
 ;; anything-rurema-at-point
 ;;(define-key global-map (kbd "C-c a r") 'anything-rurima-at-point)
 
+;; 別ウィンドウでDired
+(define-key global-map (kbd "C-x C-d") 'dired-jump-other-window)
+
+;; magit
+(global-set-key (kbd "C-x v d") 'magit-status)
+(global-set-key (kbd "C-x v L") 'magit-key-mode-popup-logging)
+(global-set-key (kbd "C-x v z") 'magit-stash)
+
+(global-unset-key (kbd "C-l"))
+(global-set-key (kbd "C-l C-l") 'recenter-top-bottom)
