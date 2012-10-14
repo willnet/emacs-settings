d52f987a05277164d0442150be090238e5065bcc
willnet <netwillnet@gmail.com>
2012-10-15 00:17:59 +0900

    Add wgrep settings

--- 30_grep.el
+++ 30_grep.el
@@ -1,3 +1,8 @@
 ;; grep結果を編集
 ;; (install-elisp-from-emacswiki "grep-edit.el")
-(require 'grep-edit)
+;; (require 'grep-edit)
+
+;; (install-elisp "https://raw.github.com/mhayashi1120/Emacs-wgrep/master/wgrep.el")
+(require 'wgrep)
+(setq wgrep-enable-key "r")
+(setq wgrep-auto-save-buffer t)
