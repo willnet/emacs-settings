629ea365e9cd391667eac350f9c6a720a1ea9999
willnet <netwillnet@gmail.com>
2012-10-15 00:04:48 +0900

    Add highlight in haml-mode

--- 50_highlight-indentation-mode.el
+++ 50_highlight-indentation-mode.el
@@ -1,3 +1,4 @@
 ;; http://blog.iss.ms/2012/03/17/095152
 (require 'highlight-indentation)
 (add-hook 'ruby-mode-hook   'highlight-indentation-current-column-mode)
+(add-hook 'haml-mode-hook   'highlight-indentation-current-column-mode)
