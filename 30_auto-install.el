abbc5aa00dc8ab1ea9e3cf7b36191e5d18c71031
willnet <netwillnet@gmail.com>
2012-10-15 00:17:47 +0900

    Modify install directory in auto-intall

--- 30_auto-install.el
+++ 30_auto-install.el
@@ -1,7 +1,7 @@
 ;; http://www.emacswiki.org/emacs/download/auto-install.el
 (require 'auto-install)
 ;; インストールディレクトリの指定
-(setq auto-install-directory "~/Dropbox/emacs-lisp/auto-install/")
+(setq auto-install-directory "~/.emacs.d/auto-install/")
 ;; EmacsWiki に登録されている elisp の名前を取得する
 (auto-install-update-emacswiki-package-name t)
 ;; install-elisp の関数を利用可能にする
