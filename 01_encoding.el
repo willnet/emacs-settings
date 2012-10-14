d199e0cf0e7428f0bf2f1e93911b8889904bf582
willnet <netwillnet@gmail.com>
2012-10-15 00:06:39 +0900

    Add cording settings

--- 01_encoding.el
+++ 01_encoding.el
@@ -15,3 +15,5 @@
 (set-terminal-coding-system     'utf-8)
 (set-keyboard-coding-system     'utf-8)
 (set-clipboard-coding-system    'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
