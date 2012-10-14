a847a6a7dcbdff74dc92ec5090cb29aa27f594cf
willnet <netwillnet@gmail.com>
2012-10-15 00:15:32 +0900

    Add ime.el

---
+++ 99_ime.el
@@ -0,0 +1,6 @@
+;; http://www.oreradio.com/2012/01/10.php
+(mac-input-method-mode)
+(setq default-input-method "MacOSX")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok24.Japanese" `title "漢")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok24.Japanese" `cursor-type 'box)
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok24.Japanese" `cursor-color "magenta")
