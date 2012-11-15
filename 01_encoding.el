35cb3ad7c7bc626dfad653191f34c3237495a6e6
willnet <netwillnet@gmail.com>
2012-11-15 10:29:01 +0900

    Set coding-system-for-write utf-8 to ruby-mode's magic comment

--- 01_encoding.el
+++ 01_encoding.el
@@ -2,15 +2,10 @@
 ;; 文字コード設定
 ;;
 
-;; (set-language-environment       "Japanese")
-;; (require 'ucs-normalize)
-;; (prefer-coding-system           'utf-8-hfs)
-;; (setq file-name-coding-system 'utf-8-hfs)
-;; (setq locale-coding-system 'utf-8-hfs)
-
 (set-language-environment       "Japanese")
 (prefer-coding-system           'utf-8-unix)
-(setq                           default-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(setq coding-system-for-write   'utf-8)
 (set-buffer-file-coding-system  'utf-8)
 (set-terminal-coding-system     'utf-8)
 (set-keyboard-coding-system     'utf-8)
