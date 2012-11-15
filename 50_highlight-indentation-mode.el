6380decf553b7c4a619e75bdaeceb9d4bf4442d0
willnet <netwillnet@gmail.com>
2012-11-15 10:30:19 +0900

    Make full highlight

--- 50_highlight-indentation-mode.el
+++ 50_highlight-indentation-mode.el
@@ -1,4 +1,9 @@
 ;; http://blog.iss.ms/2012/03/17/095152
 (require 'highlight-indentation)
+(setq highlight-indentation-offset 2)
+(set-face-background 'highlight-indentation-face "#e3e3d3")
+(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+(add-hook 'ruby-mode-hook   'highlight-indentation-mode)
+(add-hook 'haml-mode-hook   'highlight-indentation-mode)
 (add-hook 'ruby-mode-hook   'highlight-indentation-current-column-mode)
 (add-hook 'haml-mode-hook   'highlight-indentation-current-column-mode)
