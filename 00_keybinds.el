5ef5423486f4e040a2fdf4a9d49b44dfe21f8b7b
willnet <netwillnet@gmail.com>
2012-12-16 23:06:24 +0900

    Modify other-window to other-window-or-split

--- 00_keybinds.el
+++ 00_keybinds.el
@@ -19,9 +19,6 @@
 ;; C-x oの逆をC-x pに割り当てる
 (global-set-key (kbd "C-x p") (lambda () (interactive) (other-window -1)))
 
-;; ウィンドウを移る
-(define-key global-map (kbd "C-o") 'other-window)
-
 ;; rgrep
 (define-key global-map (kbd "C-x g") 'rgrep)
 
