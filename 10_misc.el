c59a7874770aedc76b7c6a39dd9f95aa2f997fdb
willnet <netwillnet@gmail.com>
2012-12-18 21:44:19 +0900

    Add winner-dwim to C-q

--- 10_misc.el
+++ 10_misc.el
@@ -9,3 +9,17 @@
 
 ;; ウィンドウを移る
 (global-set-key (kbd "C-o") 'other-window-or-split)
+
+
+(winner-mode 1)
+(defun winner-dwim (arg)
+  (interactive "P")
+  (let ((func (if arg 'winner-redo 'winner-undo)))
+    ;; funcで指定したコマンドを呼び出す
+    (let (current-prefix-arg) (call-interactively func))
+    ;; winner-dwimではなくてwinner-undo/winner-redoを実行したことにする
+    (run-with-timer 0.01 nil 'set 'last-command func)))
+
+(global-set-key (kbd "C-q") 'winner-dwim)
+;; 元のC-qを別なキーに退避させる
+(global-set-key (kbd "C-c q") 'quoted-insert)
