5e72f38df50cff8215cc536d56fd145fe38bbdaf
willnet <netwillnet@gmail.com>
2013-02-05 11:54:40 +0900

    Add web-mode and make disable rhtml-mode

--- 50_rinari.el
+++ 50_rinari.el
@@ -4,9 +4,10 @@
 
 ;; Rinari
 (require 'rinari)
-(require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-          (lambda () (rinari-launch)))
+;; (require 'rhtml-mode)
+;; (add-hook 'rhtml-mode-hook
+;;          (lambda () (rinari-launch)))
+
 (setq rinari-tags-file-name "TAGS")
 
 (setf
