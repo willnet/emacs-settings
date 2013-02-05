92a1811c927e1bec01061eab16edbf432f1500d9
willnet <netwillnet@gmail.com>
2013-02-05 11:54:40 +0900

    Add web-mode and make disable rhtml-mode

---
+++ 50_web-mode.el
@@ -0,0 +1,19 @@
+(require 'web-mode)
+
+;;; 適用する拡張子
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+
+;; なぜかC-;にC-c C-;と同じ機能が割り当てられてるので削除する
+(define-key web-mode-map (kbd "C-;") nil)
+
+;;; インデント数
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   2)
+  (setq web-mode-css-offset    2)
+  (setq web-mode-script-offset 2)
+  (setq web-mode-php-offset    2)
+  (setq web-mode-java-offset   2)
+  (setq web-mode-asp-offset    2))
+(add-hook 'web-mode-hook 'web-mode-hook)
