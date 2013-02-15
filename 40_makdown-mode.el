be584aab0f03aa613e9150a075672427ef48637a
willnet <netwillnet@gmail.com>
2013-02-15 10:43:29 +0900

    Add C-i to markdown-cycle

--- 40_makdown-mode.el
+++ 40_makdown-mode.el
@@ -1,2 +1,7 @@
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+(add-hook 'markdown-mode-hook
+  (lambda()
+    (define-key markdown-mode-map (kbd "C-i") 'markdown-cycle)
+    (hide-sublevels 2)))
