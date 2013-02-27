c372ede68dc674f5bc828a37790923b042de16cc
willnet <netwillnet@gmail.com>
2013-02-27 10:10:04 +0900

    Replace keybinds between helm-mini and helm-ls-git-ls

--- 30_helm.el
+++ 30_helm.el
@@ -1,8 +1,8 @@
 (require 'helm-config)
 ;; (helm-mode 1)
 
-(global-set-key (kbd "C-;") 'helm-ls-git-ls)
-(global-set-key (kbd "C-:") 'helm-mini)
+(global-set-key (kbd "C-;") 'helm-mini)
+(global-set-key (kbd "C-:") 'helm-ls-git-ls)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x r b")   'helm-c-pp-bookmarks)
