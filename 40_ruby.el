cf57e6db59214e242bca751f5e5a1a474a0cadd5
willnet <netwillnet@gmail.com>
2013-03-02 11:13:25 +0900

    Add .rabl to ruby-mode

--- 40_ruby.el
+++ 40_ruby.el
@@ -11,6 +11,7 @@
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
