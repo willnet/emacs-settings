f9b99863eff8316262fef7aa6865a9eb91ed39d5
willnet <netwillnet@gmail.com>
2013-03-10 15:16:08 +0900

    Add face for helm and solarized

--- 00_display.el
+++ 00_display.el
@@ -61,4 +61,11 @@
   (when (require 'color-theme-solarized)
     (color-theme-solarized-dark)))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "gray20" :underline t)))))
+
 (add-to-list 'default-frame-alist '(alpha . 90))
