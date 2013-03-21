4e2744536ed391a9c77b4d2d6261b7435dbcf3ad
willnet <netwillnet@gmail.com>
2013-03-21 22:11:07 +0900

    Update yasnippet settings
    * delete yasnippet-config settings

--- 30_yasnippet.el
+++ 30_yasnippet.el
@@ -1,17 +1,8 @@
 ;; yasnippet
-(require 'yasnippet-config)
+(require 'yasnippet)
 (require 'dropdown-list)
-(yas/global-mode 1)
-(call-interactively 'yas/reload-all) ;workaround
-;; anything-complete.elを使っているなら yas/completing-prompt のみでもよい
-(setq yas/prompt-functions
-'(yas/dropdown-prompt yas/completing-prompt yas/ido-prompt yas/no-prompt))
-
+(yas-global-mode 1)
 (setq yas-trigger-key "C-i")
-
-;; (require 'auto-complete-config)
-;; (yas/set-ac-modes)
-;; (yas/enable-emacs-lisp-paren-hack)
-;; ;; いろいろな情報源を使いたいならば (ac-config-default) にする
-;; (setq-default ac-sources '(ac-source-yasnippet))
-;; (global-auto-complete-mode 1)
+(setq yas-prompt-functions '(yas-dropdown-prompt
+                             yas-ido-prompt
+                             yas-completing-prompt))
